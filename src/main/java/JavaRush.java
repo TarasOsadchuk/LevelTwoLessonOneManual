905f1437694f2c844511317d28df1a373ba07eb8
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2021-12-08 12:32:22 +0200

    jyj

--- src/main/java/JavaRush.java
+++ src/main/java/JavaRush.java
@@ -10,13 +10,6 @@ public class JavaRush {
     public static void init(char[] symbols) {
         //напишите тут ваш код
         symbols[0] = '\u00A9';
-        symbols[1] = '\n004A';
-        symbols[2] = '\n0061';
-        symbols[3] = '\n0076';
-        symbols[4] = '\n0061';
-        symbols[5] = '\n0052';
-        symbols[6] = '\n0075';
-        symbols[7] = '\n0073';
-        symbols[8] = '\n0068';
+
     }
 }
